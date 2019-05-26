4793e26849469a607dbf267f52210b02ae227f26
Andy Durdin <me@andy.durdin.net>
2019-05-26 10:46:43 +0200

    Attempt to apply custom split normals to pointcloud vertices, but it doesn't work.

--- agnosia_tools/pointcloud.py
+++ agnosia_tools/pointcloud.py
@@ -5,15 +5,18 @@ import mathutils
 import random
 from mathutils import Vector
 
-def create_mesh_obj(name, verts):
-    mesh = bpy.data.meshes.new(name+'Mesh')
-    obj = bpy.data.objects.new(name, mesh)
-    obj.show_name = True
-    bpy.context.scene.collection.objects.link(obj)
-    mesh.from_pydata(verts, [], [])
+def create_mesh_obj(name, vertices, normals):
+    mesh = bpy.data.meshes.new(name + 'Mesh')
+    o = bpy.data.objects.new(name, mesh)
+    o.show_name = True
+    bpy.context.scene.collection.objects.link(o)
+    mesh.from_pydata(vertices, [], [])
+    mesh.calc_normals()
+    # This is supposed to set normals, but I can't get it to work.
+    # mesh.normals_split_custom_set_from_vertices(normals)
     mesh.validate(verbose=True, clean_customdata=False)
     mesh.update()
-    return obj
+    return o
 
 def calculate_radius(o):
     from math import sqrt
@@ -23,6 +26,8 @@ def calculate_radius(o):
     return radius
 
 def sphere_surface_points(radius, count, rng=random):
+    # Yield an iterable of Vector randomly distributed
+    # on the surface of a sphere with the given radius.
     from math import acos, cos, pi, sin, sqrt
     for i in range(count):
         u = rng.random()
@@ -34,23 +39,42 @@ def sphere_surface_points(radius, count, rng=random):
         z = radius * cos(phi)
         yield Vector((x, y, z))
 
+def cube_volume_points(halfwidth, count, rng=random):
+    # Yield an iterable of Vector randomly distributed
+    # within the volume of a cube with the given halfwidth.
+    for i in range(count):
+        u = rng.random()
+        v = rng.random()
+        w = rng.random()
+        x = halfwidth * u
+        y = halfwidth * v
+        z = halfwidth * w
+        yield Vector((x, y, z))
+
+def raycast_to_origin(o, pt):
+    # Raycast the object o from pt (in object space) to the origin.
+    # Return a tuple: (result, position, normal, index)
+    origin = Vector((0.0, 0.0, 0.0))
+    direction = (origin - pt).normalized()
+    return o.ray_cast(pt, direction)
+
 def create_pointcloud_from_active_object():
     o = bpy.context.active_object
     if not o: raise Exception("No object selected")
 
     count = 5000
-    points = []
+    vertices = []
+    normals = []
 
     # Sample the object spherically, uniformly.
-    origin = Vector((0.0, 0.0, 0.0))
     radius = calculate_radius(o) + 1.0
     for pt in sphere_surface_points(radius, count):
-        direction = (origin - pt).normalized()
-        (result, position, normal, index) = o.ray_cast(pt, direction)
+        result, position, normal, index = raycast_to_origin(o, pt)
         if result:
-            points.append(position)
+            vertices.append(position)
+            normals.append(normal)
 
-    cloud = create_mesh_obj(o.name + '_cloud', points)
+    cloud = create_mesh_obj(o.name + '_cloud', vertices, normals)
     cloud.select_set(True)
     o.hide_set(True)
     print(f"Created: {cloud}")
